8217b161822d52db2d9b6889637940d7940cd009
TasnimAnjum <114102739+TasnimAnjum@users.noreply.github.com>
2023-06-06 15:32:44 +0600

    Part 6

---
+++ EntityFrameWorkDatabaseFirst/Context/BlogContext.cs
@@ -0,0 +1,16 @@
+﻿using EntityFrameWorkDatabaseFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameWorkDatabaseFirst.Context
+{
+    public class BlogContext:DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
---
+++ EntityFrameWorkDatabaseFirst/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameWorkDatabaseFirst.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameWorkDatabaseFirst.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogContext db = new BlogContext();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View(db.Blogs.ToList());
+        }
+    }
+}
